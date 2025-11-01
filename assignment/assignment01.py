8bba144c3cb60ad932a89810deb18acfc50bd1a6
surajkr502 <sawsurajkumar502@gmail.com>
2025-11-01 23:00:33 +0530

    assignment

---
+++ assignment/assignment01.py
@@ -0,0 +1,5 @@
+# Give an array with some integer type value.Write a python script to short array?
+from array import *
+arr=array('i',[6,4,9,7,12,9,33,67,92,100,45,78,234,578,3556,7543])
+a_list=list(arr)
+print(sorted(a_list))
