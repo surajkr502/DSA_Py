970895027c5ed03f59843ef0b447ea1fb7c2ba5a
surajkr502 <sawsurajkumar502@gmail.com>
2025-11-07 22:22:16 +0530

    assignment

--- assignment/assignment01.py
+++ assignment/assignment01.py
@@ -1,14 +1,22 @@
-# Give an array with some integer type value.Write a python script to short array?
+# 1. Give an array with some integer type value.Write a python script to short array.
 from array import *
 # arr=array('i',[6,4,9,7,12,9,33,67,92,100,45,78,234,578,3556,7543])
 # a_list=list(arr)
 # print(sorted(a_list))
 
+# 2. Give an array with some integer type value.Write a python script to short array without using in built fn and method.
+arr=array('i',[45,74,67,25,96,4,99,23,75,96,53,28,85,30])
+n=len(arr)
+for i in range(n):
+    for j in range(0,n-i-1):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+print('sorted arr is:',arr)
 
-#Give a list of heterogenous elements.Write a python scripts to remove all the non int value from the list.
-list=(1,3,'python','@',53,'java')
-li_int=()
-for i in list:
-    if i ==int:
-        print(i)
+# 3. Give a list of heterogenous elements.Write a python scripts to remove all the non int value from the list.
+# list=(1,3,'python','@',53,'java')
+# li_int=()
+# for i in list:
+#     if i ==int:
+#         print(i)
     
--- cls_obj.py
+++ cls_obj.py
@@ -1,19 +1,47 @@
-class Test:
-    x=5
-    def __init__(self,a,b):
-        self.a=a 
-        self.b=b
-    def show(self):
-        print(self.a,self.b)
-    @staticmethod
-    def f2():
-        print('Hello')
-    @classmethod
-    def f3(cls):
-        print('cls.x')
-t1=Test(3,4)
-t2=Test(5,6)
-t1.show()
-t2.show()
-Test.f3()
-Test.f2()
+# class Test:
+#     x=5
+#     def __init__(self,a,b):
+#         self.a=a 
+#         self.b=b
+#     def show(self):
+#         print(self.a,self.b)
+#     @staticmethod
+#     def f2():
+#         print('Hello')
+#     @classmethod
+#     def f3(cls):
+#         print('cls.x')
+# t1=Test(3,4)
+# t2=Test(5,6)
+# t1.show()
+# t2.show()
+# Test.f3()
+# Test.f2()
+
+
+# Create a class Employee with atriburetes empid,name,salary and also define mathods to access properties of Employee.
+
+# class Employee:
+#     def __init__(self,empid=None,name=None,salary=None):
+#         self.empid=empid
+#         self.name=name
+#         self.salary=salary
+#     def setEmpid(self,empid):
+#         self.empid=empid
+#     def setName(self,name):
+#         self.name=name
+#     def setSalary(self,salary):
+#         self.salary=salary
+#     def getEmpid(self):
+#         return self.empid
+#     def getName(self):
+#         return self.name
+#     def getSalary(self):
+#         return self.salary
+# e1=Employee()
+# e2=Employee(1,'Rahul',40000)
+# e1.setEmpid(2)
+# e1.setName('Ramesh')
+# e1.setSalary(50000)
+# print(e2.getEmpid(),e2.getName(),e2.getSalary())
+# print(e1.getEmpid(),e1.getName(),e1.getSalary())
